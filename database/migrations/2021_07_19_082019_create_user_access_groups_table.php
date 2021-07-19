79f07108b44f7082e5d8af8c9efca9c98b67e51a
Timothy Karani <kartimothy@gmail.com>
2021-07-19 11:24:07 +0300

    Added auth migrations

--- database/migrations/2021_07_19_082019_create_user_access_groups_table.php
+++ database/migrations/2021_07_19_082019_create_user_access_groups_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateAccessPageGroupsTable extends Migration
+class CreateUserAccessGroupsTable extends Migration
 {
   /**
    * Run the migrations.
@@ -13,15 +13,15 @@ class CreateAccessPageGroupsTable extends Migration
    */
   public function up()
   {
-    Schema::create('access_page_groups', function (Blueprint $table) {
+    Schema::create('user_access_groups', function (Blueprint $table) {
       $table->id();
       $table->unsignedBigInteger('access_group_id');
-      $table->unsignedBigInteger('page_group_id');
+      $table->unsignedBigInteger('user_id');
       $table->timestamps();
       $table->softDeletes();
 
       $table->foreign('access_group_id')->references('id')->on('access_groups');
-      $table->foreign('page_group_id')->references('id')->on('page_groups');
+      $table->foreign('user_id')->references('id')->on('users');
     });
   }
 
@@ -32,6 +32,6 @@ class CreateAccessPageGroupsTable extends Migration
    */
   public function down()
   {
-    Schema::dropIfExists('access_page_groups');
+    Schema::dropIfExists('user_access_groups');
   }
 }

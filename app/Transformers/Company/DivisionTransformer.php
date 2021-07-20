a45a6826b71dd2d102b44a517ba20f04737f863e
Timothy Karani <kartimothy@gmail.com>
2021-07-20 15:10:05 +0300

    Basic hateos

--- app/Transformers/Company/DivisionTransformer.php
+++ app/Transformers/Company/DivisionTransformer.php
@@ -7,44 +7,48 @@ use League\Fractal\TransformerAbstract;
 
 class DivisionTransformer extends TransformerAbstract
 {
-    /**
-     * List of resources to automatically include
-     *
-     * @var array
-     */
-    protected $defaultIncludes = [
-        //
+  /**
+   * List of resources to automatically include
+   *
+   * @var array
+   */
+  protected $defaultIncludes = [
+    //
+  ];
+
+  /**
+   * List of resources possible to include
+   *
+   * @var array
+   */
+  protected $availableIncludes = [
+    //
+  ];
+
+  /**
+   * A Fractal transformer.
+   *
+   * @return array
+   */
+  public function transform(Division $division)
+  {
+    return [
+      "id" => (int)$division->id,
+      "name" => (string)$division->name,
+      "created_at" => (string)$division->created_at,
+      "updated_at" => (string)$division->updated_at,
+      "deleted_at" => (string)$division->deleted_at,
+
+      "links"  => [
+        [
+          'rel' => "self",
+          "href" => route("divisions.show", $division->id)
+        ],
+        [
+          "rel" => "divisions.branches",
+          "href" => route("divisions.branches.index", $division->id)
+        ]
+      ]
     ];
-
-    /**
-     * List of resources possible to include
-     *
-     * @var array
-     */
-    protected $availableIncludes = [
-        //
-    ];
-
-    /**
-     * A Fractal transformer.
-     *
-     * @return array
-     */
-    public function transform(Division $division)
-    {
-        return [
-            "id" => (int)$division->id,
-            "name" => (string)$division->name,
-            "created_at" => (string)$division->created_at,
-            "updated_at" => (string)$division->updated_at,
-            "deleted_at" => (string)$division->deleted_at,
-
-            "links"  => [
-                [
-                    'rel' => "self",
-                    "href" => route("divisions.show", $division->id)
-                ]
-            ]
-        ];
-    }
+  }
 }

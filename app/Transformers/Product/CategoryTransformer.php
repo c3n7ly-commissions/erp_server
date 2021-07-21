de045de0ae85c7bc25e8c4e6aee52b3ebf7b86d1
Timothy Karani <kartimothy@gmail.com>
2021-07-21 23:02:48 +0300

    Added transformers for (sub)categories

--- app/Transformers/Product/CategoryTransformer.php
+++ app/Transformers/Product/CategoryTransformer.php
@@ -2,37 +2,49 @@
 
 namespace App\Transformers\Product;
 
+use App\Models\Product\Category;
 use League\Fractal\TransformerAbstract;
 
 class CategoryTransformer extends TransformerAbstract
 {
-    /**
-     * List of resources to automatically include
-     *
-     * @var array
-     */
-    protected $defaultIncludes = [
-        //
+  /**
+   * List of resources to automatically include
+   *
+   * @var array
+   */
+  protected $defaultIncludes = [
+    //
+  ];
+
+  /**
+   * List of resources possible to include
+   *
+   * @var array
+   */
+  protected $availableIncludes = [
+    //
+  ];
+
+  /**
+   * A Fractal transformer.
+   *
+   * @return array
+   */
+  public function transform(Category $category)
+  {
+    return [
+      "id" => (int)$category->id,
+      "name" => (int)$category->name,
+      "created_at" => (string)$category->created_at,
+      "updated_at" => (string)$category->updated_at,
+      "deleted_at" => (string)$category->deleted_at,
+
+      "links"  => [
+        [
+          'rel' => "self",
+          "href" => route("categories.show", $category->id)
+        ],
+      ]
     ];
-    
-    /**
-     * List of resources possible to include
-     *
-     * @var array
-     */
-    protected $availableIncludes = [
-        //
-    ];
-    
-    /**
-     * A Fractal transformer.
-     *
-     * @return array
-     */
-    public function transform()
-    {
-        return [
-            //
-        ];
-    }
+  }
 }
--- app/Transformers/Product/SubCategoryTransformer.php
+++ app/Transformers/Product/SubCategoryTransformer.php
@@ -2,37 +2,49 @@
 
 namespace App\Transformers\Product;
 
+use App\Models\Product\SubCategory;
 use League\Fractal\TransformerAbstract;
 
 class SubCategoryTransformer extends TransformerAbstract
 {
-    /**
-     * List of resources to automatically include
-     *
-     * @var array
-     */
-    protected $defaultIncludes = [
-        //
+  /**
+   * List of resources to automatically include
+   *
+   * @var array
+   */
+  protected $defaultIncludes = [
+    //
+  ];
+
+  /**
+   * List of resources possible to include
+   *
+   * @var array
+   */
+  protected $availableIncludes = [
+    //
+  ];
+
+  /**
+   * A Fractal transformer.
+   *
+   * @return array
+   */
+  public function transform(SubCategory $subcategory)
+  {
+    return [
+      "id" => (int)$subcategory->id,
+      "name" => (int)$subcategory->name,
+      "created_at" => (string)$subcategory->created_at,
+      "updated_at" => (string)$subcategory->updated_at,
+      "deleted_at" => (string)$subcategory->deleted_at,
+
+      "links"  => [
+        [
+          'rel' => "self",
+          "href" => route("subcategories.show", $subcategory->id)
+        ],
+      ]
     ];
-    
-    /**
-     * List of resources possible to include
-     *
-     * @var array
-     */
-    protected $availableIncludes = [
-        //
-    ];
-    
-    /**
-     * A Fractal transformer.
-     *
-     * @return array
-     */
-    public function transform()
-    {
-        return [
-            //
-        ];
-    }
+  }
 }

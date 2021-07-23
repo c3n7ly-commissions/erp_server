45965570d63080a403d1214093e206b3421bfc14
Timothy Karani <kartimothy@gmail.com>
2021-07-23 09:51:01 +0300

    Added model and migration

---
+++ app/Models/Partners/Supplier/Supplier.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models\Partners\Supplier;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Supplier extends Model
+{
+  use HasFactory, SoftDeletes;
+
+  const ACTIVE = "active";
+  const INACTIVE = "inactive";
+  const REJECTED = "rejected";
+
+  protected $dates = [
+    'deleted_at'
+  ];
+
+  protected $fillable = [
+    "name",
+    "email",
+    "telephone",
+    "postal_address",
+    "physical_address",
+    "tax_id",
+    "status",
+    "payment_terms",
+    "credit_limit",
+  ];
+}
---
+++ database/migrations/2021_07_23_064022_create_suppliers_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use App\Models\Partners\Supplier\Supplier;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSuppliersTable extends Migration
+{
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    Schema::create('suppliers', function (Blueprint $table) {
+      $table->id();
+      $table->string('name');
+      $table->string('email');
+      $table->string("telephone");
+      $table->string("postal_address");
+      $table->string("physical_address");
+      $table->string("tax_id");
+      $table->string("status")->default(Supplier::INACTIVE);
+      $table->unsignedDecimal("payment_terms", 12, 2);
+      $table->unsignedDecimal("credit_limit", 12, 2);
+      $table->timestamps();
+      $table->softDeletes();
+    });
+  }
+
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down()
+  {
+    Schema::dropIfExists('suppliers');
+  }
+}

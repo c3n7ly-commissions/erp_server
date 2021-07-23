df7370a6b0e6bcb623c127b73f551f97c3290101
Timothy Karani <kartimothy@gmail.com>
2021-07-23 09:27:31 +0300

    done with product level crud

--- app/Http/Controllers/Company/BranchProductLevelController.php
+++ app/Http/Controllers/Company/BranchProductLevelController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\ApiController;
 use App\Models\Company\Branch;
 use App\Models\Product\ProductLevel;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 
 class BranchProductLevelController extends ApiController
 {
@@ -16,8 +17,8 @@ class BranchProductLevelController extends ApiController
    */
   public function index(Branch $branch)
   {
-    $producLevels = $branch->productLevels;
-    return $this->showAll($producLevels);
+    $productLevels = $branch->productLevels;
+    return $this->showAll($productLevels);
   }
 
   /**
@@ -26,9 +27,23 @@ class BranchProductLevelController extends ApiController
    * @param  \Illuminate\Http\Request  $request
    * @return \Illuminate\Http\Response
    */
-  public function store(Request $request)
+  public function store(Request $request, Branch $branch)
   {
-    //
+    $rules = [
+      "minimum_level" => "required|numeric",
+      "maximum_level" => "required|numeric",
+      "reorder_level" => "required|numeric",
+      "quantity" => "required|numeric",
+      "product_id" => "required|numeric|integer|exists:products,id",
+    ];
+
+    $request->validate($rules);
+    $data = $request->all();
+
+    $data['branch_id'] = $branch->id;
+    $productLevel = ProductLevel::create($data);
+
+    return $this->showOne($productLevel);
   }
 
   /**
@@ -38,10 +53,46 @@ class BranchProductLevelController extends ApiController
    * @param  \App\Models\Company\Branch  $branch
    * @return \Illuminate\Http\Response
    */
-  public function update(Request $request, Branch $branch)
+  public function update(Request $request, Branch $branch, ProductLevel $productLevel)
   {
-    //
+    $rules = [
+      "minimum_level" => "numeric",
+      "maximum_level" => "numeric",
+      "reorder_level" => "numeric",
+      "quantity" => "numeric",
+      "product_id" => "numeric|integer|exists:products,id",
+    ];
+
+    $request->validate($rules);
+    $this->checkRelationship($branch, $productLevel);
+
+    $productLevel->fill($request->only([
+      "minimum_level",
+      "maximum_level",
+      "reorder_level",
+      "quantity",
+      "product_id",
+    ]));
+
+    if ($productLevel->isClean()) {
+      return $this->errorResponse('you need to specify different values to update', 422);
+    }
+
+    $productLevel->save();
+
+    return $this->showOne($productLevel);
   }
+
+  public function checkRelationship(Branch $branch, ProductLevel $productLevel)
+  {
+    if ($branch->id != $productLevel->branch_id) {
+      throw new HttpException(
+        422,
+        "there exists no relationship between the passed branch and productLevel"
+      );
+    }
+  }
+
 
   /**
    * Remove the specified resource from storage.
@@ -49,8 +100,12 @@ class BranchProductLevelController extends ApiController
    * @param  \App\Models\Company\Branch  $branch
    * @return \Illuminate\Http\Response
    */
-  public function destroy(Branch $branch)
+  public function destroy(Branch $branch, ProductLevel $productLevel)
   {
-    //
+    $this->checkRelationship($branch, $productLevel);
+
+    $productLevel->delete();
+
+    return $this->showOne($productLevel);
   }
 }
